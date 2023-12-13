f69cde93d8ae17846c09c4cb257d6400b9fac49d
Your Name <you@example.com>
2023-12-12 23:36:24 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>I Love men!!!</>;
+}
+
+export default App;
