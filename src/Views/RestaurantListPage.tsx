ef3d7faa4686d7f86538b97bd246719394c299f7
Your Name <you@example.com>
2023-12-13 21:59:42 -0500

    added gallery functionality

--- src/Views/RestaurantListPage.tsx
+++ src/Views/RestaurantListPage.tsx
@@ -14,12 +14,24 @@ import { motion } from "framer-motion";
 import { pageVariants, pageTransition } from "./framer";
 import { foods } from "./foodsTestData";
 import StarRating from "../components/StarRating";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 //TODO add hyper link to restaurant card, allowing user to access restaurant page
 //TODO Functionality to click on card and display review description
 //TODO add field for category and filter by category
 //TODO add field for price and filter by price
 //TODO add field for location
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+
 
 const RestaurantListPage = () => {
   return (
@@ -38,12 +50,18 @@ const RestaurantListPage = () => {
           {foods.map((restaurant) => (
             <GridItem key={restaurant.id}>
               <Box borderWidth="1px" borderRadius="md" overflow="hidden">
-                <Image
-                  src={restaurant.image}
-                  alt={restaurant.name}
-                  h={200}
-                  objectFit="cover"
-                />
+                <Slider {...settings}>
+                  {restaurant.images.map((image, index) => (
+                    <div key={index}>
+                      <Image
+                        src={image}
+                        alt={`${restaurant.name} image ${index + 1}`}
+                        h={200}
+                        objectFit="cover"
+                      />
+                    </div>
+                  ))}
+                </Slider>
                 <Box p={4}>
                   <Heading as="h2" size="md" mb={2}>
                     {restaurant.name}
@@ -53,9 +71,9 @@ const RestaurantListPage = () => {
                 <Flex p={4}>
                   <Spacer />
                   <VStack spacing={2}>
-                <StarRating rating={restaurant.rating} /> 
-                {/* Add buttons or actions here */}
-              </VStack>
+                    <StarRating rating={restaurant.rating} />
+                    {/* Add buttons or actions here */}
+                  </VStack>
                 </Flex>
               </Box>
             </GridItem>
