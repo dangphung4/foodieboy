87b263e4e8ecde769cd8b319b77f41306e1bde35
Your Name <you@example.com>
2023-12-14 09:58:33 -0500

    slider bug with random restuarant

--- src/Views/FoodPickerPage.tsx
+++ src/Views/FoodPickerPage.tsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import { Box, Flex, Heading, Text, VStack, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Image,
+  VStack,
+  Button,
+  Spacer,
+} from "@chakra-ui/react";
 import FoodButton from "../components/FoodButton";
 import { pageVariants, pageTransition } from "./framer";
 import { motion } from "framer-motion";
 import { foods } from "./foodsTestData";
 import { Restaurant } from "../components/types/Restaurant";
+import StarRating from "../components/StarRating";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const FoodPickerPage: React.FC = () => {
   const [selectedRestaurant, setSelectedRestaurant] =
     useState<Restaurant | null>(null);
 
-  const handleFoodButtonClick = () => {
-    const randomIndex = Math.floor(Math.random() * foods.length);
-    console.log(randomIndex);
-    setSelectedRestaurant(foods[randomIndex]);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
+  const handleFoodButtonClick = () => {
+    const randomIndex = Math.floor(Math.random() * foods.length);
+    setSelectedRestaurant(foods[randomIndex]);
+  };
+console.log(selectedRestaurant);
   return (
     <motion.div
       initial="initial"
@@ -30,22 +50,24 @@ const FoodPickerPage: React.FC = () => {
             <Heading as="h1" size="xl">
               Food Picker Page
             </Heading>
-          </Box>
-          <Box>
             <Text>Where should I eat today?</Text>
-            <FoodButton label="Foodie me!" onClick={handleFoodButtonClick}/>
+            <FoodButton label="Foodie me!" onClick={handleFoodButtonClick} />
           </Box>
           {selectedRestaurant && (
-            <Box>
-              <Heading as="h2" size="lg">
-                {selectedRestaurant.name}
-              </Heading>
-              <Text>{selectedRestaurant.description}</Text>
+            <Box borderWidth="1px" borderRadius="md" overflow="hidden" mt={4}>
               <Image
                 src={selectedRestaurant.images[0]}
-                alt={selectedRestaurant.name}
+                alt={`${selectedRestaurant.name} image`}
+                h={200}
+                objectFit="cover"
               />
-              {/* Add more details as needed */}
+              <Box p={4}>
+                <Heading as="h2" size="md" mb={2}>
+                  {selectedRestaurant.name}
+                </Heading>
+                <Text>{selectedRestaurant.description}</Text>
+                <StarRating rating={selectedRestaurant.rating} />
+              </Box>
             </Box>
           )}
         </VStack>
