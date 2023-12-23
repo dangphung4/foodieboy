6a6414cf131cb02ad89fbbfe1ed4636263110fc6
Ale <alemp1@LAPTOP-UPGCVSAB>
2023-12-22 23:34:02 -0500

    csvlandingpage

--- src/AnimatedRoutes.tsx
+++ src/AnimatedRoutes.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from 'framer-motion';
-import HomePage from './Views/HomePage';
+import { AnimatePresence } from "framer-motion";
+import HomePage from "./Views/HomePage";
 import FoodPickerPage from "./Views/FoodPickerPage";
-import RestaurantListPage from './Views/RestaurantListPage';
+import RestaurantListPage from "./Views/RestaurantListPage";
+import CsvLandingPage from "./Views/CsvLandingPage";
 
 const AnimatedRoutes = () => {
   const location = useLocation();
@@ -12,7 +13,8 @@ const AnimatedRoutes = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
         <Route path="/food" element={<FoodPickerPage />} />
-        <Route path='/food/list' element={<RestaurantListPage />} />
+        <Route path="/food/list" element={<RestaurantListPage />} />
+        <Route path="/csv" element={<CsvLandingPage />} />
       </Routes>
     </AnimatePresence>
   );
